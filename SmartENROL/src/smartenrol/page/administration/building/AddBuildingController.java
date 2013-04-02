20801ec490d41d6b70c6ec6469a25270eb898182
Jeremy Wallace <jeremy@smartenrol.ca>
2013-04-01 20:51:01 -0700

    Added Building DAO and Controller

--- SmartENROL/src/smartenrol/page/administration/building/AddBuildingController.java
+++ SmartENROL/src/smartenrol/page/administration/building/AddBuildingController.java
@@ -9,5 +9,31 @@ package smartenrol.page.administration.building;
  * @author Jeremy
  */
 public class AddBuildingController {
+ 
+    Building newBuilding;
+    BuildingDAO buildingDAO;
     
+    // Form Elements
+    TextBox buildingNameTextBox;
+    TextBox address1;
+    TextBox address2;
+    TextBox city;
+    TextBox postalCode;
+    TextBox notes;
+    
+    public verifyPostalCode() {
+         
+    }
+    
+    public submitForm(Action event) {
+        
+        newBuilding.address1 = address1.getText();
+        newBuilding.address1 = address2.getText();
+        newBuilding.address1 = address2.getText();
+        newBuilding.address1 = address2.getText();
+        newBuilding.address1 = address2.getText();
+        
+        buildingDAO.addBuilding(newBuilding);
+    }
+            
 }
---
+++ SmartENROL/src/smartenrol/page/login/LoginController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartenrol.page.login;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class LoginController {
+    
+}
