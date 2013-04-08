9182970df774ced934b811b3f370ce749096c864
Haijun Qiao <haijunq@gmail.com>
2013-04-08 00:16:10 -0700

    timetable controller

--- SmartENROL/src/smartenrol/page/timetable/TimetableController.java
+++ SmartENROL/src/smartenrol/page/timetable/TimetableController.java
@@ -4,10 +4,12 @@
  */
 package smartenrol.page.timetable;
 
-import java.util.Calendar;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
 import jfxtras.labs.scene.control.Agenda;
+import jfxtras.labs.scene.control.Agenda.CalendarRange;
+import org.joda.time.DateTime;
 import smartenrol.page.AbstractController;
 
 /**
@@ -17,7 +19,7 @@ import smartenrol.page.AbstractController;
 public class TimetableController extends AbstractController {
      
     @FXML BorderPane innerContent;
-     Calendar test;
+
      
      public void init() {
         
@@ -25,7 +27,22 @@ public class TimetableController extends AbstractController {
     
      public void openAgenda() {
             final Agenda smartTimetable = new Agenda();
+//            smartTimetable.setMouseTransparent(true);
+//            smartTimetable.with
+            smartTimetable.setDisplayedCalendar(new DateTime(2013, 4, 1, 8, 0).toGregorianCalendar());
+            smartTimetable.appointments().add(
+                new Agenda.AppointmentImpl()
+                        .withStartTime(new DateTime(2013, 4, 3, 12, 0).toGregorianCalendar())
+                        .withEndTime(new DateTime(2013, 4, 3, 14, 0).toGregorianCalendar())
+                        .withSummary("Course \nInstructor \nFSC-101")
+//                        .withDescription("Instructor Name")
+//                        .withLocation("FSC-101")
+                        .withAppointmentGroup(new Agenda.AppointmentGroupImpl().withStyleClass("group16"))
+                    );
+            
             smartTimetable.setMinHeight(400);  
+
+            
             innerContent.setCenter(smartTimetable);
             
      }
