7584ed960e25e2d500139bd76f2887f31d94e1dc
MrAtheist <MrAtheist@192.168.0.101>
2013-04-08 11:08:28 -0700

    loadSiderbar added in PageController

--- SmartENROL/src/smartenrol/page/PageController.java
+++ SmartENROL/src/smartenrol/page/PageController.java
@@ -19,7 +19,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
 import smartenrol.page.*;
 import smartenrol.page.administration.building.*;
 import smartenrol.page.administration.course.*;
@@ -39,63 +39,179 @@ import smartenrol.dao.CourseDAO;
 import smartenrol.model.Course;
 public class PageController extends AbstractController
 {
-   
-    @FXML private BorderPane contentArea;
-    @FXML private TextField searchField;
-    
-    @FXML private ImageView dashboardIcon;
-    @FXML private ImageView myProfileIcon;
-    @FXML private ImageView timetableIcon;
-    @FXML private ImageView myProgramIcon;
-    @FXML private ImageView universalSearchIcon;
-    @FXML private Text welcomeText;
-    
-    private final CourseDAO coursedao = new CourseDAO();
-    @Autowired private DashboardController dashboardController;
-    @Autowired private AddBuildingController addBuildingController;
-    @Autowired private AddCourseController addCourseController;
-    @Autowired private AddDepartmentController addDepartmentController;
-    @Autowired private AddFacultyController addFacultyController;
-    @Autowired private AddProgramController addProgramController;
-    @Autowired private AddSectionController addSectionController;
-    @Autowired private TimetableController timetableController;
-    @Autowired private StudentSidebarController studentSidebarController;
-    @Autowired private InstructorSidebarController instructorSidebarController;
-    @Autowired private AdministratorSidebarController administratorSidebarController;
-    @Autowired private CoursePageController coursePageController;
-    @Autowired private CourseSidebarController courseSidebarController;
-    @Autowired private SearchController searchController;
-    @Autowired private LoginController loginController;
-    @Autowired private MyProgramPageController myProgramPageController;
-    @Autowired private MyProfileController myProfileController;
+	
+	@FXML private BorderPane contentArea;
+	@FXML private TextField searchField;
+	
+	@FXML private ImageView dashboardIcon;
+	@FXML private ImageView myProfileIcon;
+	@FXML private ImageView timetableIcon;
+	@FXML private ImageView myProgramIcon;
+	@FXML private ImageView universalSearchIcon;
+	@FXML private Text welcomeText;
+	
+	private final CourseDAO coursedao = new CourseDAO();
+	@Autowired private DashboardController dashboardController;
+	@Autowired private AddBuildingController addBuildingController;
+	@Autowired private AddCourseController addCourseController;
+	@Autowired private AddDepartmentController addDepartmentController;
+	@Autowired private AddFacultyController addFacultyController;
+	@Autowired private AddProgramController addProgramController;
+	@Autowired private AddSectionController addSectionController;
+	@Autowired private TimetableController timetableController;
+	@Autowired private StudentSidebarController studentSidebarController;
+	@Autowired private InstructorSidebarController instructorSidebarController;
+	@Autowired private AdministratorSidebarController administratorSidebarController;
+	@Autowired private CoursePageController coursePageController;
+	@Autowired private CourseSidebarController courseSidebarController;
+	@Autowired private SearchController searchController;
+	@Autowired private LoginController loginController;
+	@Autowired private MyProgramPageController myProgramPageController;
+	@Autowired private MyProfileController myProfileController;
+	
+	public void init() {
+		if (UserSession.getInstance().isSignedIn()) {
+			welcomeText.setText("Welcome back, "+getUserSession().getCurrentUser().getFullName());
+		}
+		
+	}
+	
+	@Autowired
+	public void setCoursePageController (  CoursePageController coursePageController ){
+		this.coursePageController = coursePageController;
+	}
+	
+	@FXML
+	public void navDashboard()
+	{
+		contentArea.setCenter(dashboardController.getView());
+		
+//		contentArea.setRight(studentSidebarController.getView());
+		loadSidebar();
+	}
 
-    public void init() {
-        if (UserSession.getInstance().isSignedIn()) {
-            welcomeText.setText("Welcome back, "+getUserSession().getCurrentUser().getFullName());
-        }
-    
-    }
-    
-    @Autowired
-    public void setCoursePageController (  CoursePageController coursePageController ){
-      this.coursePageController = coursePageController;
-    }
-   
-    @FXML
-    public void navDashboard()
-    {
-        contentArea.setCenter(dashboardController.getView());
+	@FXML
+	public void loadProfile()
+	{
+		contentArea.setCenter(myProfileController.getView());
+		myProfileController.loadProfile();
+	}
+	
+	@FXML
+	public void navAddBuilding()
+	{
+		contentArea.setCenter(addBuildingController.getView());
+	}
+	
+	@FXML
+	public void navAddCourse()
+	{
+		
+		contentArea.setCenter(addCourseController.getView());
+	}
+	
+	@FXML
+	public void navAddDepartment()
+	{
+		contentArea.setCenter(addDepartmentController.getView());
+	}
+	
+	@FXML
+	public void navAddProgram()
+	{
+		contentArea.setCenter(addProgramController.getView());
+	}
+	
+	@FXML
+	public void navAddSection()
+	{
+		contentArea.setCenter(addSectionController.getView());
+	}
+	
+	@FXML
+	public void navAddFaculty()
+	{
+		
+		contentArea.setCenter(addFacultyController.getView());
+		
+	}
+	
+	@FXML
+	public void navCoursePage()
+	{
+		//        coursePageController = new CoursePageController("cics", 520);
+		coursePageController.init();
+		contentArea.setCenter(coursePageController.getView());
+		courseSidebarController.load(coursedao.getCourseByID("CICS",520));
+		courseSidebarController.init();
+		contentArea.setRight(courseSidebarController.getView());
+	}
+	
+	@FXML
+	public void navTimetable()
+	{
+		contentArea.setRight(null);
+		contentArea.setCenter(timetableController.getView());
+		timetableController.openAgenda();
+	}
+	
+	@FXML
+	public void navMyProgramPage()
+	{
+		contentArea.setCenter(myProgramPageController.getView());
+	}
+	
+	@FXML
+	public void dashboardIconOnHover() {
+		dashboardIcon.setImage(new Image("../images/se-dashboard-icon-hit.png"));
+	}
+	
+	@FXML
+	public void search() {
+		contentArea.setRight(null);
+		searchController.search(searchField.getText());
+		contentArea.setCenter(searchController.getView());
+	}
+	
+	@FXML
+	public void logout() {
+	}
+	
+	@FXML
+	public void showFaq(ActionEvent event)
+	{
+		String link="http://www.smartenrol.ca";
+		try {
+			Desktop.getDesktop().browse(URI.create(link));
+		} catch (IOException ex) {
+			Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	@FXML
+	public void showAbout(ActionEvent event)
+	{
+		String link="http://www.smartenrol.ca";
+		try {
+			Desktop.getDesktop().browse(URI.create(link));
+		} catch (IOException ex) {
+			Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	@FXML
+	public void showManual(ActionEvent event)
+	{
+		String link="http://www.smartenrol.ca";
+		try {
+			Desktop.getDesktop().browse(URI.create(link));
+		} catch (IOException ex) {
+			Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	public void loadSidebar() {
 
-        contentArea.setRight(studentSidebarController.getView());
-    }
-     @FXML
-    public void loadProfile()
-    {
-        contentArea.setCenter(myProfileController.getView());
-        myProfileController.loadProfile();
-    }
-	/*	switch (getUserSession().getCurrentUser().getUsertype()) {
-
+		switch (getUserSession().getCurrentUser().getUsertype()) {
+			
 			case "Student":
 				contentArea.setRight(studentSidebarController.getView());
 				break;
@@ -105,119 +221,6 @@ public class PageController extends AbstractController
 			case "Administrator":
 				contentArea.setRight(administratorSidebarController.getView());
 				break;
-    	}*/
-
-    
-    @FXML
-    public void navAddBuilding()
-    {
-        contentArea.setCenter(addBuildingController.getView());
-    }
- 
-    @FXML
-    public void navAddCourse()
-    {
-        
-        contentArea.setCenter(addCourseController.getView());
-    }    
-    
-    @FXML
-    public void navAddDepartment()
-    {
-        contentArea.setCenter(addDepartmentController.getView());
-    }   
-    
-    @FXML
-    public void navAddProgram()
-    {
-        contentArea.setCenter(addProgramController.getView());
-    }
-    
-    @FXML
-    public void navAddSection()
-    {
-        contentArea.setCenter(addSectionController.getView());
-    }   
-    
-    @FXML
-    public void navAddFaculty()
-    {
-        
-        contentArea.setCenter(addFacultyController.getView());
-        
-    }
-    
-    @FXML
-    public void navCoursePage()
-    {
-//        coursePageController = new CoursePageController("cics", 520);
-        coursePageController.init();
-        contentArea.setCenter(coursePageController.getView());
-        courseSidebarController.load(coursedao.getCourseByID("CICS",520));
-        courseSidebarController.init();
-        contentArea.setRight(courseSidebarController.getView());
-    } 
-
-    @FXML
-    public void navTimetable()
-    {
-        contentArea.setRight(null);
-        contentArea.setCenter(timetableController.getView());
-        timetableController.openAgenda();
-    }
-    
-    @FXML
-    public void navMyProgramPage()
-    {
-        contentArea.setCenter(myProgramPageController.getView());
-    }
-    
-    @FXML
-    public void dashboardIconOnHover() {
-        dashboardIcon.setImage(new Image("../images/se-dashboard-icon-hit.png"));
-    }
-    
-    @FXML
-    public void search() {
-        contentArea.setRight(null);
-        searchController.search(searchField.getText());
-        contentArea.setCenter(searchController.getView());
-    }
-    
-    @FXML
-    public void logout() {
-    }
-//<<<<<<< HEAD
-    @FXML
-public void showFaq(ActionEvent event)
-{
-String link="http://www.smartenrol.ca";
-        try {
-            Desktop.getDesktop().browse(URI.create(link));
-        } catch (IOException ex) {
-            Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		}
+	}
 }
-        @FXML
-public void showAbout(ActionEvent event)
-{
-String link="http://www.smartenrol.ca";
-        try {
-            Desktop.getDesktop().browse(URI.create(link));
-        } catch (IOException ex) {
-            Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-}
-            @FXML
-public void showManual(ActionEvent event)
-{
-String link="http://www.smartenrol.ca";
-        try {
-            Desktop.getDesktop().browse(URI.create(link));
-        } catch (IOException ex) {
-            Logger.getLogger(PageController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-}
-}
-//}
-//
