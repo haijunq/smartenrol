ca83b6e3a13da5e35eeabb5c013e6ad519a321fa
Jeremy Wallace <jeremy@smartenrol.ca>
2013-04-08 16:46:27 -0700

    Connected ClassList Controller

--- SmartENROL/src/smartenrol/page/classlist/ClassListController.java
+++ SmartENROL/src/smartenrol/page/classlist/ClassListController.java
@@ -4,11 +4,18 @@
  */
 package smartenrol.page.classlist;
 
+import smartenrol.page.SmartEnrolController;
+
 /**
  *
  * @author Jeremy
  * This page also allows administrator to add/remove a student to the class (special approval). 
  */
-public class ClassListController {
+public class ClassListController extends SmartEnrolController {
+
+    @Override
+    public void init() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
