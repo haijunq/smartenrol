cdd56801e57491a480ef34f272711de94e13facd
Haijun Qiao <haijunq@gmail.com>
2013-04-08 02:09:54 -0700

    Timetable can be displayed now.

--- SmartENROL/src/smartenrol/page/timetable/TimetableController.java
+++ SmartENROL/src/smartenrol/page/timetable/TimetableController.java
@@ -10,6 +10,9 @@ import javafx.util.Callback;
 import jfxtras.labs.scene.control.Agenda;
 import jfxtras.labs.scene.control.Agenda.CalendarRange;
 import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import smartenrol.dao.StudentSectionDAO;
+import smartenrol.model.Timetable;
 import smartenrol.page.AbstractController;
 
 /**
@@ -17,34 +20,51 @@ import smartenrol.page.AbstractController;
  * @author Jeremy
  */
 public class TimetableController extends AbstractController {
-     
+    private DateTime fixDay;
+    private Timetable currentTimetable;
+    
     @FXML BorderPane innerContent;
-
      
-     public void init() {
-        
+    public void init() {
+        fixDay = new DateTime(2013, 3, 31, 0, 0);
+        currentTimetable = new StudentSectionDAO().getStudentTimetable(80013010);
+//        currentTimetable = new StudentSectionDAO().getInstructorTimetable(80012002);
      }
     
-     public void openAgenda() {
-            final Agenda smartTimetable = new Agenda();
-//            smartTimetable.setMouseTransparent(true);
-//            smartTimetable.with
-            smartTimetable.setDisplayedCalendar(new DateTime(2013, 4, 1, 8, 0).toGregorianCalendar());
+    /**
+     * Open and display the Agenda view. 
+     */
+    public void openAgenda() {
+        final Agenda smartTimetable = new Agenda();
+        smartTimetable.setMouseTransparent(true);
+        smartTimetable.setDisplayedCalendar(fixDay.toGregorianCalendar());
+        
+        for (int i = 0; i < currentTimetable.getSectionNodeList().size(); i++) 
             smartTimetable.appointments().add(
                 new Agenda.AppointmentImpl()
-                        .withStartTime(new DateTime(2013, 4, 3, 12, 0).toGregorianCalendar())
-                        .withEndTime(new DateTime(2013, 4, 3, 14, 0).toGregorianCalendar())
-                        .withSummary("Course \nInstructor \nFSC-101")
-//                        .withDescription("Instructor Name")
-//                        .withLocation("FSC-101")
+                        .withStartTime(this.calSectionTime(currentTimetable.getSectionNodeList().get(i).getDay(), currentTimetable.getSectionNodeList().get(i).getStartTime()).toGregorianCalendar())
+                        .withEndTime(this.calSectionTime(currentTimetable.getSectionNodeList().get(i).getDay(), currentTimetable.getSectionNodeList().get(i).getEndTime()).toGregorianCalendar())
+                        .withSummary(currentTimetable.getSectionNodeList().get(i).getIdDepartment() + " " + currentTimetable.getSectionNodeList().get(i).getIdCourse() + "\n" + currentTimetable.getSectionNodeList().get(i).getClassRoom())
+    //                        .withDescription("Instructor Name")
+    //                        .withLocation("FSC-101")
                         .withAppointmentGroup(new Agenda.AppointmentGroupImpl().withStyleClass("group16"))
                     );
-            
-            smartTimetable.setMinHeight(400);  
+        
+        smartTimetable.setMinHeight(400);  
 
-            
-            innerContent.setCenter(smartTimetable);
+        innerContent.setCenter(smartTimetable);
             
      }
     
+    /**
+     * Calculate the DateTime that will be displayed in the timetable view according to DayOfWeek and LocalTime.
+     * @param day
+     * @param time
+     * @return 
+     */
+    private DateTime calSectionTime(int day, LocalTime time) {
+        DateTime dt = fixDay;
+        dt = dt.plusDays(day).plusHours(time.getHourOfDay()).plusMinutes(time.getMinuteOfHour()).plusSeconds(time.getSecondOfMinute());
+        return dt;
+    }
 }
