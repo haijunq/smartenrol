ae548462055683ae8a76956fb4c493f6ca898171
Ashwin <>
2013-04-09 11:10:40 -0700

    Added New Methods

---
+++ SmartENROL/src/smartenrol/page/administration/faculty/FacultyException.java
@@ -0,0 +1,13 @@
+package smartenrol.page.administration.faculty;
+
+import smartenrol.SmartEnrolException;
+
+public class FacultyException extends SmartEnrolException
+{
+
+	public FacultyException(final String errorMessage)
+	{
+		super(errorMessage);
+	}
+
+}
---
+++ SmartENROL/src/smartenrol/page/administration/faculty/IFacultyServiceConstants.java
@@ -0,0 +1,12 @@
+package smartenrol.page.administration.faculty;
+
+public interface IFacultyServiceConstants
+{
+	public static final String ID_FACULTY = "ID_FACULTY";
+	public static final String NAME = "NAME";
+	public static final String DESCRIPTION = "DESCRIPTION";
+	public static final String HEAD_OFFICE_LOCATION = "HEAD_OFFICE_LOCATION";
+	public static final String MAIN_PHONE = "MAIN_PHONE";
+	public static final String DEAN_ID = "DEAN_ID";
+	public static final String MAIN_CONTACT_ID = "MAIN_CONTACT_ID";
+}
