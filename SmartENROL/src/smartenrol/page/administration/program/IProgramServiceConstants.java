c07f362f6419133a2eb9123be52b0f03ff283b30
Ashwin <>
2013-04-09 11:10:54 -0700

    Added New Methods

---
+++ SmartENROL/src/smartenrol/page/administration/program/IProgramServiceConstants.java
@@ -0,0 +1,10 @@
+package smartenrol.page.administration.program;
+
+public interface IProgramServiceConstants {
+
+    public static final String ID_PROGRAM = "ID_PROGRAM";
+    public static final String ID_DEPARTMENT = "ID_DEPARTMENT";
+    public static final String PROGRAM_NAME = "PROGRAM_NAME";
+    public static final String PROGRAM_DESCRIPTION = "PROGRAM_DESCRIPTION";
+    public static final String TOTAL_GRADE = "TOTAL_GRADE";  
+}
---
+++ SmartENROL/src/smartenrol/page/administration/program/ProgramException.java
@@ -0,0 +1,10 @@
+package smartenrol.page.administration.program;
+
+import smartenrol.SmartEnrolException;
+
+public class ProgramException extends SmartEnrolException {
+
+    public ProgramException(final String errorMessage) {
+        super(errorMessage);
+    }
+}
