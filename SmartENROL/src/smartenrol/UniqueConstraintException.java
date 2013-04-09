47d68c28dac469f7061291e788ff6976f5936d6a
Ashwin <>
2013-04-08 19:27:58 -0700

    New Exception classes added to handle unique constraint

---
+++ SmartENROL/src/smartenrol/UniqueConstraintException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartenrol;
+
+/**
+ *
+ * @author Ashwin
+ */
+public class UniqueConstraintException extends SmartEnrolException {
+
+    public UniqueConstraintException(final String errorMessage) {
+        super(errorMessage);
+    }
+}
