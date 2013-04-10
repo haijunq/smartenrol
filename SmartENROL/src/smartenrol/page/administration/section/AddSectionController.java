69fc0bdb680030a8c63f53edecd4f9f1c571f89c
MrAtheist <MrAtheist@192.168.0.101>
2013-04-09 23:58:11 -0700

    updated basic gui structures + fxids

--- SmartENROL/src/smartenrol/page/administration/section/AddSectionController.java
+++ SmartENROL/src/smartenrol/page/administration/section/AddSectionController.java
@@ -4,6 +4,11 @@
  */
 package smartenrol.page.administration.section;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import smartenrol.page.SmartEnrolController;
 
 /**
@@ -11,7 +16,29 @@ import smartenrol.page.SmartEnrolController;
  * @author Jeremy
  */
 public class AddSectionController extends SmartEnrolController {
+	
+	@FXML ComboBox fxCourse;
+	@FXML ComboBox fxInstructor;
+	@FXML ComboBox fxDay;
+	@FXML ComboBox fxStartTime;
+	@FXML ComboBox fxLength;
+	
+	@FXML TableView fxSectionTable;
+	@FXML Slider fxNumOfStudents;
+
+	@Override
     public void init() {
         
     }
+
+	@FXML
+    private void submitForm(MouseEvent event) throws Exception {
+
+	}
+
+    @FXML
+    private void addTime(MouseEvent event) throws Exception {
+
+	}
+
 }
