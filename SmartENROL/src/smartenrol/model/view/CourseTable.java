c43cbbbb58c395dd06d8d88350196897d71554c8
MrAtheist <MrAtheist@192.168.0.101>
2013-04-21 13:31:22 -0700

    WORKING VERSION

--- SmartENROL/src/smartenrol/model/view/CourseTable.java
+++ SmartENROL/src/smartenrol/model/view/CourseTable.java
@@ -12,50 +12,61 @@ import smartenrol.model.Course;
  * @author Jeremy
  */
 public class CourseTable {
-    
-    private SimpleStringProperty idDepartment;
-    private SimpleIntegerProperty idCourse;
-    private SimpleStringProperty name;
-    private SimpleFloatProperty credit;
-
-    public CourseTable(Course course) {
-        this.idDepartment = new SimpleStringProperty(course.getIdDepartment());
-        this.idCourse = new SimpleIntegerProperty(course.getIdCourse());
-        this.name = new SimpleStringProperty(course.getCourseName());
-        this.credit = new SimpleFloatProperty(course.getCredits());
-    }
-
-    
-    
-    
-    /**
-     * @return the Department
-     */
-    public String getIdDepartment() {
-        return idDepartment.get();
-    }
-
-    /**
-     * @return the Course
-     */
-    public int getIdCourse() {
-        return idCourse.get();
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name.get();
-    }
-
-    /**
-     * @return the credit
-     */
-    public float getCredit() {
-        return credit.get();
-    }
-    
-   
-    
+	
+	private SimpleStringProperty idDepartment;
+	private SimpleIntegerProperty idCourse;
+	private SimpleStringProperty name;
+	private SimpleFloatProperty credit;
+	private SimpleStringProperty course;
+	
+	public CourseTable(Course course) {
+		this.idDepartment = new SimpleStringProperty(course.getIdDepartment());
+		this.idCourse = new SimpleIntegerProperty(course.getIdCourse());
+		this.name = new SimpleStringProperty(course.getCourseName());
+		this.credit = new SimpleFloatProperty(course.getCredits());
+	}
+	
+	public CourseTable(String idDepartment, int idCourse) {
+		this.idDepartment = new SimpleStringProperty(idDepartment);
+		this.idCourse = new SimpleIntegerProperty(idCourse);
+		this.course = new SimpleStringProperty(idDepartment + " " + String.valueOf(idCourse));
+	}
+	
+	/**
+	 * @return the department + course id
+	 */
+	public String getCourse() {
+		return course.get();
+	}
+	
+	/**
+	 * @return the Department
+	 */
+	public String getIdDepartment() {
+		return idDepartment.get();
+	}
+	
+	/**
+	 * @return the Course
+	 */
+	public int getIdCourse() {
+		return idCourse.get();
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name.get();
+	}
+	
+	/**
+	 * @return the credit
+	 */
+	public float getCredit() {
+		return credit.get();
+	}
+	
+	
+	
 }
