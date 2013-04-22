7f24c36199f2ba4604cbc09335ea868196be8b15
Terry <>
2013-04-22 15:58:06 -0700

    special request handling - version 1.0 done

--- SmartENROL/src/smartenrol/page/dashboard/DashboardController.java
+++ SmartENROL/src/smartenrol/page/dashboard/DashboardController.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import org.javafxdata.control.TableViewFactory;
 import smartenrol.dao.MessageDAO;
+import smartenrol.dao.StudentCoursePermissionDAO;
 
 import smartenrol.model.Message;
 
@@ -43,19 +44,28 @@ public class DashboardController extends SmartEnrolController {
 
     @FXML
     public void init() {
-        processbtn.setVisible(false);
-        messageList.clear();
+        processbtn.setDisable(true);
+    
         User.Type usertype = getUserSession().getCurrentUser().getUsertype();
         currentUser = getUserSession().getCurrentUser();
         if (usertype.equals(User.Type.ADMINISTRATOR)) {
             setSidebarEnabled(false);
+            processbtn.setVisible(true);
         } else {
             setSidebarEnabled(true);
+            processbtn.setVisible(false);
         }
         welcomeMsg.setText("Welcome back, " + currentUser.getGivenName() + "!");
+        populateMessageList();
+        setMessageTableView();
+        innerContent.setCenter(tableView);
+       
+
+    }
+
+    private void populateMessageList() {
         messageList.clear();
         messageList = messagedao.getMessageByRecepient(currentUser.getIdUser());
-
         if (!messageList.isEmpty()) {
             messageTableList.clear();
             for (Message m : messageList) {
@@ -65,52 +75,56 @@ public class DashboardController extends SmartEnrolController {
         } else {
             messageTableList = new ArrayList<>();
             noMsg.setText("Your message box is empty!");
-
         }
-        setMessageTableView();
-        innerContent.setCenter(tableView);
-
+      
     }
-
+    
     public void processRequest()
     {
-        System.out.println("Processing...");
-        String[] result = (((MessageTable) tableView.getSelectionModel().getSelectedItem()).parseEnrolRequest());
-                        for (int i = 0; i < result.length; i++) {
-                            System.out.println(result[i]);
-                        }
+//        System.out.println("Processing...");
+//        String[] result = (((MessageTable) tableView.getSelectionModel().getSelectedItem()).parseEnrolRequest());
+//                        for (int i = 0; i < result.length; i++) {
+//                            System.out.println(result[i]);
+//                        }
+        MessageTable selected=(MessageTable) tableView.getSelectionModel().getSelectedItem();
+        String[] data=selected.parseEnrolRequest();
+        int r=0;
+        if (data.length==4)
+        {
+            r=new StudentCoursePermissionDAO().addStudentSpecialPermission(selected.getSenderID(), data[1], Integer.parseInt(data[2]), data[0]);
+        }
+        if (r==1)
+        {
+            System.out.println(new MessageDAO().markMessageAsProcessed(selected.getId()));
+            refreshTable();
+        }
+        
+    }
+    
+    private void refreshTable()
+    {
+        populateMessageList();
+        tableView.setItems(FXCollections.observableList(messageTableList));
+        innerContent.setCenter(tableView);
     }
     
     private void setMessageTableView() {
-//        tableView=null;
-
         tableView = new TableView<>();
 
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+       
         TableColumn dateCol = new TableColumn("Date");
         TableColumn idCol = new TableColumn("From ID");
         TableColumn nameCol = new TableColumn("From Name");
         TableColumn messageCol = new TableColumn("Message");
+        TableColumn statusCol = new TableColumn("Status");
+        
         messageCol.setMinWidth(200);
-
-        tableView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent me) {
-                if (me.getClickCount() > 0) {
-//                    navigator.loadSelectedItem(tableView, "program");
-                    if (((MessageTable) tableView.getSelectionModel().getSelectedItem()).isSpeicialApproval())
-                    {
-                       processbtn.setVisible(true);
-                    }
-                    else
-                    {
-                       processbtn.setVisible(false);
-                    }
-                   
-                }
-            }
-        });
-
+        dateCol.setMinWidth(80);
+        idCol.setMinWidth(80);
+        nameCol.setMinWidth(80);
+        statusCol.setMinWidth(80);
+        tableView.setItems(FXCollections.observableList(messageTableList));
+   
         dateCol.setCellValueFactory(
                 new PropertyValueFactory<MessageTable, String>("date"));
         idCol.setCellValueFactory(
@@ -119,11 +133,36 @@ public class DashboardController extends SmartEnrolController {
                 new PropertyValueFactory<MessageTable, String>("senderName"));
         messageCol.setCellValueFactory(
                 new PropertyValueFactory<MessageTable, String>("message"));
-
-        tableView.setItems(FXCollections.observableList(messageTableList));
-
-        tableView.getColumns().setAll(dateCol, idCol, nameCol, messageCol);
-
+        statusCol.setCellValueFactory(
+                new PropertyValueFactory<MessageTable, String>("status"));
+     
+        tableView.getColumns().setAll(statusCol, dateCol, idCol, nameCol, messageCol);
         tableView.setEditable(false);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+       
+        if (!messageTableList.isEmpty()) {
+            tableView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent me) {
+                    if (me.getClickCount() > 0) {
+                        MessageTable selected = (MessageTable) tableView.getSelectionModel().getSelectedItem();
+                        //speical approval
+                        if (!selected.isProcessed()) {
+                            if (selected.isSpeicialApproval()) {
+                                processbtn.setDisable(false);
+                            } else {
+                                processbtn.setDisable(true);
+                            }
+                            //change status to read
+                            if (selected.isNew()) {
+                                int r = new MessageDAO().markMessageAsRead(selected.getId());
+                                refreshTable();
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        
     }
 }
